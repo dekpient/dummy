dceee8fa3273e8804d4f9737700f603cd3e7329a
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2018-11-05 23:35:45 +1300

    Massive list of contacts

--- App.js
+++ App.js
@@ -7,7 +7,15 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {NativeModules, NativeEventEmitter, Platform, StyleSheet, Text, View, TouchableOpacity, SectionList, ScrollView} from 'react-native';
+
+import JSONTree from 'react-native-json-tree'
+import Contacts from 'react-native-contacts';
+import slowlog from 'react-native-slowlog';
+import * as R from "ramda";
+
+const _Contacts = NativeModules.Contacts;
+const eventEmitter = new NativeEventEmitter(_Contacts);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,16 +24,88 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+// datat1979
+
 type Props = {};
-export default class App extends Component<Props> {
+
+type State = {
+  contacts: object
+};
+
+let subscription;
+
+export default class App extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    slowlog(this, /.*/, { verbose: true });
+    this.state = { showRaw: false, showList: false, changed: false, changedData: null };
+  }
+
+  componentWillMount() {
+    const comp = this;
+    subscription = eventEmitter.addListener(
+      'ContactsChanged',
+      (changedData) => comp.setState({ changed: "true", changedData })
+    );
+  }
+
+  componentWillUnmount() {
+    subscription.remove();
+  }
+
   render() {
+    const { contacts, showRaw, showList, changed, changedData, subscribed = false } = this.state;
+    const transformToSection = R.pipe(
+      R.groupBy(contact => Math.floor(parseInt(contact.middleName) / 10)),
+      R.toPairs,
+      R.map(([title, data]) => ({ title, data }))
+    );
+
+    const comp = this;
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity style={styles.instructions} onPress={() => Contacts.subscribeToUpdates((err, subscribed) => comp.setState({ subscribed }))}>
+          <Text style={{ fontWeight: "bold" }}>Subscribe to changes (Subscribed: {subscribed}, Changed: {changed})</Text>
+          {changedData && <Text>{changedData}</Text>}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.instructions} onPress={this._getContacts.bind(this)}>
+          <Text style={{ fontWeight: "bold" }}>Get Native Contacts</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.instructions} onPress={() => this.setState({ showRaw: !showRaw })}>
+          <Text style={{ fontWeight: "bold" }}>Toggle JSON</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.instructions} onPress={() => this.setState({ showList: !showList })}>
+          <Text style={{ fontWeight: "bold" }}>Toggle List</Text>
+        </TouchableOpacity>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          {showRaw && <JSONTree data={contacts} />}
+          {showList && <SectionList
+            renderItem={({item, index, section}) => <Text key={index}>{item.givenName}</Text>}
+            renderSectionHeader={({section: {title}}) => (
+              <Text style={{fontWeight: 'bold'}}>{title}</Text>
+            )}
+            sections={transformToSection(contacts)}
+            keyExtractor={(item, index) => item.middleName}
+          />}
+        </ScrollView>
       </View>
     );
+  }
+
+  async _getContacts() {
+    const contacts = await new Promise((resolve, reject) => {
+      Contacts.getAllWithoutPhotos((err, contacts) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(contacts);
+      });
+    });
+    this.setState({ contacts });
   }
 }
 
@@ -46,4 +126,8 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  contentContainer: {
+    paddingVertical: 20,
+    width: "100%"
+  }
 });
